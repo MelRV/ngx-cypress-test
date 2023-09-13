e560ae35d7f17642a75e5e83e04cccf4a144eced
MelissaRV <melissa.rvillegas206@gmail.com>
2023-09-13 15:47:04 -0600

    cambios2.0

---
+++ cypress/e2e/testWithPageObjects.js
@@ -0,0 +1,20 @@
+import { onFormLayoutPage } from "../support/page_objects/formLayoutPage"
+import { navigateTo } from "../support/page_objects/navigationPage"
+
+describe('Test with Page Objects', () => {
+
+  beforeEach('Open application', () => {
+    cy.visit('/')
+  })
+  it('Verify the navigation across the pages', () => {
+    navigateTo.formLayoutPage()
+    navigateTo.datePickerPage()
+    navigateTo.smartTablePage()
+    navigateTo.toasterPage()
+    navigateTo.toolTipPage()
+  })
+  it('Should submit Inline and Basic form and select tomorrow date in the calendar', () => {
+    navigateTo.formLayoutPage()
+    onFormLayoutPage.submitInlineFormWithNameAndEmail('Artem', 'test@test.com')
+  })
+})
---
+++ cypress/support/page_objects/navigationPage.js
@@ -0,0 +1,40 @@
+
+function selectGroupMenuItem (groupName){
+  cy.contains('a', groupName).then( menu =>{
+    cy.wrap(menu).find('.expand-state g g').invoke('attr', 'data-name').then( attr => {
+      if( attr.includes('left')){
+        cy.wrap(menu).click()
+      }
+    })
+  })
+}
+
+export class NavigationPage {
+
+  formLayoutPage(){
+    selectGroupMenuItem('Form')
+    cy.contains('Form Layout').click()
+  }
+
+  datePickerPage(){
+    /*cy.contains('Forms').click() (este también funciona bien) */
+    selectGroupMenuItem('Form')
+    cy.contains('Datepicker').click()
+  }
+
+  toasterPage(){
+    selectGroupMenuItem('Modal & Overlays')
+    cy.contains('Toastr').click()
+  }
+
+  smartTablePage(){
+    selectGroupMenuItem('Tables & Data')
+    cy.contains('Smart Table').click()
+  }
+  toolTipPage(){
+    selectGroupMenuItem('Modal & Overlays')
+    cy.contains('Tooltip').click()
+  }
+}
+
+export const navigateTo = new NavigationPage()
